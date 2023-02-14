f2acca8a6dfcdeaaac1aca6444eedcd11a44727a
KALP PRAJAPATI <99316165+kalp224@users.noreply.github.com>
2023-02-14 22:36:58 +0530

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
-
 import { FooterOverlay, Newsletter } from '../../components';
 import { images } from '../../constants';
 import './Footer.css';
